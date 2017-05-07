c3dfb3948ceb8e94949d8a3a3b4f6cddec2ed263
Wen Hao <wenhao@126.com>
2017-05-07 22:50:18 +0800

    [Wen Hao] - parameterized topbar.

---
+++ src/plugins/parameterTopbar/index.js
@@ -0,0 +1,9 @@
+import Topbar from "./parameter_topbar.jsx"
+
+export default function () {
+  return {
+    components: {
+      ParameterTopbar
+    }
+  }
+}
--- src/standalone/index.js
+++ src/standalone/index.js
@@ -1,13 +1,15 @@
 import StandaloneLayout from "./layout"
 import "../style/main.scss"
 
-import TopbarPlugin from "plugins/topbar"
+// import TopbarPlugin from "plugins/topbar"
 import ConfigsPlugin from "plugins/configs"
+import ParameterTopbarPlugin from "plugins/topbar"
 
 // the Standalone preset
 
 let preset = [
-  TopbarPlugin,
+  // TopbarPlugin,
+  ParameterTopbarPlugin,
   ConfigsPlugin,
   () => {
     return {
